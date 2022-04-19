39c42c7a32d1c97f5178f1751ebd33f124e0f7f6
JahanzebKhan98 <80027665+JahanzebKhan98@users.noreply.github.com>
2022-04-19 18:07:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import requests
 try:
   url="https://www.google.com/search?q=jennkins+PermissionError%3A+%5BErrno+13%5D+Permission+denied%3A+%27%2Fusr%2Flocal%2Fbin%2Fpip%27&oq=jennkins+PermissionError%3A+%5BErrno+13%5D+Permission+denied%3A+%27%2Fusr%2Flocal%2Fbin%2Fpip%27&aqs=chrome..69i57.3980j0j9&sourceid=chrome&ie=UTF-8"
   rep = requests.get(url)
+  print ('hello jk sucessfully launched')
   print (rep)
 except:
   print ('error occured jk')
